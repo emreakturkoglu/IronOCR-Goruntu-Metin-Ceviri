a0eadbd9e1b9f61621df3711f1a350da962c417e
emreakturkoglu <61273958+emreakturkoglu@users.noreply.github.com>
2020-04-27 15:44:28 +0300

    Initial commit
    IronOCR-Goruntu-Metin-Ceviri

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace OCR_tarama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dosyasec = new System.Windows.Forms.Button();
+            this.ocr_text = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(40, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dosya Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Dosyasec);
+            // 
+            // dosyasec
+            // 
+            this.dosyasec.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dosyasec.Location = new System.Drawing.Point(215, 93);
+            this.dosyasec.Name = "dosyasec";
+            this.dosyasec.Size = new System.Drawing.Size(200, 54);
+            this.dosyasec.TabIndex = 1;
+            this.dosyasec.Text = "Metne Dönüştür";
+            this.dosyasec.UseVisualStyleBackColor = true;
+            this.dosyasec.Click += new System.EventHandler(this.ocrdonusum);
+            // 
+            // ocr_text
+            // 
+            this.ocr_text.Location = new System.Drawing.Point(514, 67);
+            this.ocr_text.Multiline = true;
+            this.ocr_text.Name = "ocr_text";
+            this.ocr_text.Size = new System.Drawing.Size(392, 500);
+            this.ocr_text.TabIndex = 2;
+            this.ocr_text.Text = "OCR";
+            this.ocr_text.TextChanged += new System.EventHandler(this.ocr_text_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::OCR_tarama.Properties.Resources.OCR;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(84, 190);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 354);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::OCR_tarama.Properties.Resources._9AZKH1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(959, 624);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ocr_text);
+            this.Controls.Add(this.dosyasec);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button dosyasec;
+        private System.Windows.Forms.TextBox ocr_text;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronOcr;
+
+namespace OCR_tarama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap image = new Bitmap("deneme.jpg");
+        private void Dosyasec(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if(file.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                image = new Bitmap(file.FileName);
+                pictureBox1.Image = image;
+            }
+
+        }
+
+        private void ocrdonusum(object sender, EventArgs e)
+        {
+            var ocr = new AutoOcr();
+            ocr_text.Text=ocr.Read(image).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            OpenFileDialog pdf = new OpenFileDialog();
+            pdf.ShowDialog();
+
+        }
+
+        private void ocr_text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
